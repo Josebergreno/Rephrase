79fba68b17aa488e36421cb70bbb865aedf5079d
Joseph Bergren <bergren.bergren@yahoo.com>
2022-12-21 10:40:02 -0600

    created react app for rephrase project, added remote repository

---
+++ src/components/bottom/Bottom.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Bottom.module.css";
+
+const Bottom = () => {
+  return <div className={styles["bottom"]}>Bottom</div>;
+};
+
+export default Bottom;
---
+++ src/components/main-content/MainContent.js
@@ -0,0 +1,7 @@
+import styles from "./MainContent.module.css";
+
+const MainContent = () => {
+  return <div className={styles["main-content--container"]}>MainContent</div>;
+};
+
+export default MainContent;
