79b1144062381d7dba6e28e71cb84fe374555773
Joseph Bergren <bergren.bergren@yahoo.com>
2022-12-22 15:51:13 -0600

    began writing funcitonality for data fetching, filtering for punctuation

--- src/components/main-content/MainContent.js
+++ src/components/main-content/MainContent.js
@@ -4,27 +4,46 @@ import styles from "./MainContent.module.css";
 const MainContent = () => {
   const ref = useRef("");
   const [textContent, setTextContent] = useState("");
+
+  //   if (ref.current.value === undefined) {
+  //     console.log("empty ref");
+  //   } else {
+  //     const punctuationMap = ref.current.value
+  //       .toLowerCase()
+  //       .split(" ")
+  //       .map((val) =>
+  //         val.at(-1).toUpperCase() === val.at(-1)
+  //           ? val.replace(/[^A-Za-z0-9]/g, "")
+  //           : val
+  //       );
+
   const clickHandler = (e) => {
     e.preventDefault();
     setTextContent(ref.current.value);
-    async function fetchSynonyms() {
-      const synonymsUrl = `https://wordsapiv1.p.rapidapi.com/words/${ref.current.value}/synonyms`;
+    const map = ref.current.value
+      .split(" ")
+      .map((val) =>
+        val.at(-1) === val.at(-1).toUpperCase() ? val.slice(0, -1) : val
+      );
+    console.log(map);
+    // async function fetchSynonyms() {
+    //   const synonymsUrl = `https://wordsapiv1.p.rapidapi.com/words/${ref.current.value}/synonyms`;
 
-      const options = {
-        method: "GET",
-        headers: {
-          "X-RapidAPI-Key":
-            "b123967923msh19bc23c13dee912p17e95bjsn19b6a6d19eb1",
-          "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com",
-        },
-      };
+    //   const options = {
+    //     method: "GET",
+    //     headers: {
+    //       "X-RapidAPI-Key":
+    //         "b123967923msh19bc23c13dee912p17e95bjsn19b6a6d19eb1",
+    //       "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com",
+    //     },
+    //   };
 
-      fetch(synonymsUrl, options)
-        .then((res) => res.json())
-        .then((json) => console.log(json))
-        .catch((err) => console.error("error:" + err));
-    }
-    fetchSynonyms();
+    //   fetch(synonymsUrl, options)
+    //     .then((res) => res.json())
+    //     .then((json) => console.log(json))
+    //     .catch((err) => console.error("error:" + err));
+    // }
+    // fetchSynonyms();
   };
   return (
     <div className={styles["main-content--container"]}>
