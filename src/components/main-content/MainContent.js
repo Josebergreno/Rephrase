1ce1eab92985b7d80fd9968802a30a1fb951b04e
Joseph Bergren <bergren.bergren@yahoo.com>
2022-12-22 21:29:38 -0600

    commented code for main content component

--- src/components/main-content/MainContent.js
+++ src/components/main-content/MainContent.js
@@ -4,41 +4,52 @@ import { Select, MenuItem, SvgIcon } from "@mui/material";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 
 const MainContent = () => {
-  const [clicked, setClicked] = useState(false);
-  const dropdownHandler = () => {
-    console.log("clicked");
-    clicked === false ? setClicked(true) : setClicked(false);
-  };
+  // ref for output element
   const ref = useRef("");
+  // state handler for output container
   const [textContent, setTextContent] = useState("");
+  // dropdown clicked state
+  const [dropdownClicked, setDropdownClicked] = useState(false);
+  //   dropdown handler
+  const dropdownHandler = () => {
+    dropdownClicked === false
+      ? setDropdownClicked(true)
+      : setDropdownClicked(false);
+  };
 
+  //   rephrase handler
   const clickHandler = (e) => {
     e.preventDefault();
     setTextContent(ref.current.value);
-    const indivArr = ref.current.value.split(" ");
-    console.log(indivArr);
-
-    const fetchSynonyms = async () => {
-      const options = {
-        method: "GET",
-        headers: {
-          "X-RapidAPI-Key":
-            "b123967923msh19bc23c13dee912p17e95bjsn19b6a6d19eb1",
-          "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com",
-        },
-      };
-      const urlArr = indivArr.map(
-        (val) => `https://wordsapiv1.p.rapidapi.com/words/${val}/synonyms`
-      );
-
-      const arrayOfResponses = await Promise.all(
-        urlArr.map((url) => fetch(url, options).then((res) => res.json()))
-      );
-      console.log(arrayOfResponses);
-    };
-    fetchSynonyms();
+    return fetchSynonyms();
   };
 
+  //   async function
+  const fetchSynonyms = async () => {
+    // current text content broken into array
+    const indivArr = ref.current.value.split(" ");
+    console.log(indivArr);
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "b123967923msh19bc23c13dee912p17e95bjsn19b6a6d19eb1",
+        "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com",
+      },
+    };
+    // array of urls based off of indivArr
+    const urlArr = indivArr.map(
+      (val) => `https://wordsapiv1.p.rapidapi.com/words/${val}/synonyms`
+    );
+    // calls api for each member of array
+    const arrayOfResponses = await Promise.all(
+      urlArr.map((url) => fetch(url, options).then((res) => res.json()))
+    );
+    console.log(arrayOfResponses);
+    return arrayOfResponses;
+  };
+  // map a dropdown for each element of an array
+  //   implement ability to filter based off of typed of word
+  //   extract dropdown into individual component, and map it below to create multiple dropdowns based off of selected filters (verb, noun, adjective)
   return (
     <div className={styles["main-content--container"]}>
       <div className={styles["input-container"]}>
@@ -71,7 +82,8 @@ const MainContent = () => {
       </div>
       <div className={styles["output-container"]}>
         <div className={styles["output-field"]}>
-          {textContent}
+          {/* {textContent} */}
+
           <div className={styles["dropdown-container"]}>
             <div onClick={dropdownHandler} className={styles["select"]}>
               dropdown
@@ -80,11 +92,11 @@ const MainContent = () => {
                   width: "2vh",
                   height: "3vh",
                   marginLeft: "2px",
-                  borderLeft: "solid black 1px",
+                  borderLeft: "solid rgb(197, 193, 192) 1px",
                 }}
               />
             </div>
-            {clicked === true && (
+            {dropdownClicked === true && (
               <>
                 <div className={styles["option"]}>hello</div>
                 <div className={styles["option"]}>hello</div>
